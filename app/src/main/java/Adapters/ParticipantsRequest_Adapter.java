490a42e3f14780499642481b0c39d153d8d7c5f4
Computer <abdullahhossaincse18@gmail.com>
2021-07-16 02:43:25 +0600

    Update

--- app/src/main/java/Adapters/ParticipantsRequest_Adapter.java
+++ app/src/main/java/Adapters/ParticipantsRequest_Adapter.java
@@ -1,6 +1,7 @@
 package Adapters;
 
 import android.content.Context;
+import android.content.Intent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -11,18 +12,17 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import com.bumptech.glide.Glide;
 import com.yaga909.contestyard.R;
+import Organizer.View_Participant_Info_Activity;
 
-import java.util.ConcurrentModificationException;
 import java.util.List;
 
 import Organizer.Participant_List;
-import USER.Status_List;
 import de.hdodenhof.circleimageview.CircleImageView;
 
 public class ParticipantsRequest_Adapter extends RecyclerView.Adapter<ParticipantsRequest_Adapter.ParticipantRequest_CustomViewHolder> {
 
-Context context;
-List<Participant_List> participant_list;
+    Context context;
+    List<Participant_List> participant_list;
 
     public ParticipantsRequest_Adapter(Context context, List<Participant_List> participant_list) {
         this.context = context;
@@ -30,19 +30,24 @@ List<Participant_List> participant_list;
     }
 
     @Override
-    public ParticipantRequest_CustomViewHolder onCreateViewHolder(@NonNull  ViewGroup parent, int viewType) {
+    public ParticipantRequest_CustomViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
         LayoutInflater inflater = LayoutInflater.from(parent.getContext());
-        View view = inflater.inflate(R.layout.row_view_requests, parent, false);
+        View view = inflater.inflate(R.layout.row_org_accept, parent, false);
         return new ParticipantRequest_CustomViewHolder(view);
     }
 
     @Override
-    public void onBindViewHolder(@NonNull  ParticipantsRequest_Adapter.ParticipantRequest_CustomViewHolder holder, int position) {
+    public void onBindViewHolder(@NonNull ParticipantsRequest_Adapter.ParticipantRequest_CustomViewHolder holder, int position) {
         String url = "http://projecttech.xyz/contest_yard/comp_banners/";
-       Participant_List list = participant_list.get(position);
+        Participant_List list = participant_list.get(position);
         holder.name.setText(list.getComp_name());
-        holder.status.setText(list.getComp_status());
-        Glide.with(context).load(url+list.getComp_image()).into(holder.image);
+        Glide.with(context).load(url + list.getComp_image()).into(holder.image);
+        holder.view.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                context.startActivity(new Intent(context, View_Participant_Info_Activity.class));
+            }
+        });
 
     }
 
@@ -51,15 +56,16 @@ List<Participant_List> participant_list;
         return participant_list.size();
     }
 
-    class ParticipantRequest_CustomViewHolder extends RecyclerView.ViewHolder{
+    class ParticipantRequest_CustomViewHolder extends RecyclerView.ViewHolder {
         CircleImageView image;
-        TextView name, status;
+        TextView name, view;
 
         public ParticipantRequest_CustomViewHolder(@NonNull View itemView) {
             super(itemView);
             image = itemView.findViewById(R.id.comp_image);
             name = itemView.findViewById(R.id.comp_name);
-            status = itemView.findViewById(R.id.comp_status);
+            view = itemView.findViewById(R.id.viewBTN);
+
 
         }
     }
---
+++ app/src/main/java/Organizer/View_Participant_Info_Activity.java
@@ -0,0 +1,16 @@
+package Organizer;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.yaga909.contestyard.R;
+
+public class View_Participant_Info_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view_participant_info);
+    }
+}
